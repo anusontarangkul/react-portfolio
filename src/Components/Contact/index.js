95d45361f0344330f3188b95eb9b39590c6c24e9
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-11-18 09:54:02 -0800

    update contact

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -3,7 +3,10 @@ import './style.css';
 
 function Contact() {
     return (
-        null
+        <main className="background-page">
+            <h1 className="contact-h1">Contact Me</h1>
+            <p className="contact-p">If you would like to chat about a work opportunity, please email me at anusontarangkul.d@gmail.com and I will respond within 1 business day.</p>
+        </main>
     );
 }
 
